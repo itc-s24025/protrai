0018714ec381bf3744fe107402e9d8fbdcc23254
itc-s24025 <itc-s24025@std.it-college.ac.jp>
2024-06-10 06:24:37 +0900

    if_kadai

---
+++ if2.py
@@ -0,0 +1,9 @@
+#s24025
+#条件式の練習 if2.py
+
+score=60
+if score >= 80:
+    print("やったね！")
+    print("次もこの調子だ")
+else:
+    print("ざんねんでした")
---
+++ if_kadai.py
@@ -0,0 +1,13 @@
+#s24025
+#score評価プログラム
+
+
+if score >= 60:
+        prnit("可")
+if score >= 70:
+        print("良")
+if score >= 80:
+        print("優")
+if score < 60:
+        print("不可")
+
