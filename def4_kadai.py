ecf5db6511cb252adb318bf45ad4f4e18ee9ec08
itc-s24025 <itc-s24025@std.it-college.ac.jp>
2024-06-17 06:53:14 +0900

    def4_kadai.py

--- def4_kadai.py
+++ def4_kadai.py
@@ -2,10 +2,13 @@
 #main関数練習プログラミング
 
 import random
+
+#ランダムでkujiの中の一つを返す関数
 def omikuji():
     kuji=["大吉","中吉","小吉","吉"]
     return random.choice(kuji)
 
+#エントリーポイントの定義
 def main():
     kekka = omikuji()
     print("結果は", kekka ,"です。")
